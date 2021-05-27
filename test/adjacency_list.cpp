bfe010d688b2fbac58750db7e3fc4801848cf309
Thanh Tuan Tran <15673727+thanhtuantran@users.noreply.github.com>
2021-05-27 16:10:20 +0200

    new test for empty graph

--- test/adjacency_list.cpp
+++ test/adjacency_list.cpp
@@ -6,3 +6,9 @@ TEST_CASE("Adjacency list construction with n vertices")
     netz::AdjacencyList g(5);
     REQUIRE(g.num_vertices() == 5);
 }
+
+TEST_CASE("Adjacecny list construction with 0 vertices")
+{
+	netz::AdjacencyList g(0);
+	REQUIRE(g.num_vertices() == 0);
+}
