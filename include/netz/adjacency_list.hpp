d0b3e3f0848bcc7210de075cca5775fdaaf022cd
Duc Thanh Tran <duc.thanh.tran.git@protonmail.com>
2021-07-01 23:11:52 +0200

    Adding edges and vertices to graphs
    * Use seperate std::vector container for edges and vertices
    * Add edges (undirected) to graph by using vertex indices
    * Add vertices to graph

--- include/netz/adjacency_list.hpp
+++ include/netz/adjacency_list.hpp
@@ -7,24 +7,47 @@
 namespace netz
 {
 
-struct OutEdge
-{
-    std::size_t target;
-};
-
 class AdjacencyList
 {
 public:
-    AdjacencyList() noexcept { }
-    AdjacencyList(const std::size_t numVertices) : m_adjList(numVertices) { }
+
+    struct Vertex {};
+
+    struct Edge
+    {
+        std::size_t source;
+        std::size_t target;
+        Edge(const std::size_t source, const std::size_t target) : source(source), target(target) { }
+    };
+
+    AdjacencyList() noexcept : m_vertices(), m_edges() { }
+    AdjacencyList(const std::size_t numVertices) : m_vertices(numVertices), m_edges() { }
 
     std::size_t num_vertices() const
     {
-        return m_adjList.size();
+        return m_vertices.size();
+    }
+
+    std::size_t num_edges() const
+    {
+        return m_edges.size();
+    }
+
+    std::size_t add_vertex()
+    {
+        m_vertices.push_back(Vertex{});
+        return m_vertices.size() - 1;
+    }
+
+    Edge add_edge(const std::size_t source, const std::size_t target)
+    {
+        m_edges.emplace_back(source, target);
+        return m_edges.back();
     }
 
 private:
-    std::vector<std::vector<OutEdge>> m_adjList;
+    std::vector<Vertex> m_vertices;
+    std::vector<Edge> m_edges;
 };
 
 } // namespace netz
--- test/adjacency_list.cpp
+++ test/adjacency_list.cpp
@@ -3,12 +3,34 @@
 
 TEST_CASE("Adjacency list construction with n vertices")
 {
-    netz::AdjacencyList g(5);
+    netz::AdjacencyList g{5};
     REQUIRE(g.num_vertices() == 5);
+    REQUIRE(g.num_edges() == 0);
 }
 
 TEST_CASE("Adjacency list construction with 0 vertices")
 {
-	netz::AdjacencyList g();
-	REQUIRE(g.num_vertices() == 0);
+    netz::AdjacencyList g{};
+    REQUIRE(g.num_vertices() == 0);
+    REQUIRE(g.num_edges() == 0);
+}
+
+TEST_CASE("Add vertices and return vertex index")
+{
+    netz::AdjacencyList g{};
+    const auto numVertices = 12;
+    for(auto i = 0; i < numVertices; ++i)
+    {
+        REQUIRE(i == g.add_vertex());
+    }
+    REQUIRE(g.num_vertices() == numVertices);
+}
+
+TEST_CASE("Add edges to graph")
+{
+    netz::AdjacencyList g{4};
+    const auto edge = g.add_edge(0, 1);
+    REQUIRE(edge.source == 0);
+    REQUIRE(edge.target == 1);
+    REQUIRE(g.num_edges() == 1);
 }
